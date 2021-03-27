00a01910b0b9d6c2aa4387e2a8fdaa8effad2b10
azharimm <azhari.marzan@gmail.com>
2021-03-27 13:04:43 +0700

    init

---
+++ src/controllers/indexController.js
@@ -0,0 +1,10 @@
+const { json, errorJson } = require('../utils/response');
+
+exports.index = (req, res) => {
+    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    
+    return json(res, {
+        maintainer: 'Azhari Muhammad M <azhari.marzan@gmail.com>',
+        source: 'https://github.com/azharimm/google-keyword-trend-api',
+    });
+}
